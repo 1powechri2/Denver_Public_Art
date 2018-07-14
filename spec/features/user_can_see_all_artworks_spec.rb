2b707d7df19522ddaad7ca7fbe2ea8b3c5fe6c18
Chris Powell <chris.ian.powell@gmail.com>
2018-07-14 17:17:06 -0600

    adds link to google maps for each location on index page

--- spec/features/user_can_see_all_artworks_spec.rb
+++ spec/features/user_can_see_all_artworks_spec.rb
@@ -9,11 +9,11 @@ describe 'A user can see all artworks on index page' do
       visit artworks_path
 
       expect(page).to have_content("Title: #{art_one.title}")
-      expect(page).to have_content("Title: #{art_one.artist}")
-      expect(page).to have_content("Title: #{art_one.location}")
+      expect(page).to have_content("Artist: #{art_one.artist}")
+      expect(page).to have_content("Location: #{art_one.location}")
       expect(page).to have_content("Title: #{art_two.title}")
-      expect(page).to have_content("Title: #{art_two.artist}")
-      expect(page).to have_content("Title: #{art_two.location}")
+      expect(page).to have_content("Artist: #{art_two.artist}")
+      expect(page).to have_content("Location: #{art_two.location}")
     end
   end
 end
