6eb8186ad65bdfa64bd3ede07e5ec15b63abb90d
Chris Powell <chris.ian.powell@gmail.com>
2018-07-16 19:23:35 -0600

    adds user logout function and index page styling

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+
 describe 'registration flow' do
   it 'submits registration succesfully' do
     username = 'Jans_Arp'
@@ -15,5 +16,38 @@ describe 'registration flow' do
     click_on 'Create User'
 
     expect(page).to have_content("Welcome, #{username}!")
+  end
+  it 'can log in as a registered user' do
+    user = User.create(username: "Chros", password: "4p9ybaos")
+    visit '/'
+    click_on "I already have an account"
+
+    expect(current_path).to eq(login_path)
+    fill_in "username", with: user.username
+    fill_in "password", with: user.password
+
+    click_on "Log In"
+
+    expect(current_path).to eq(user_path(user))
+
+    expect(page).to have_content("Welcome, #{user.username}")
+    expect(page).to have_content("Log Out")
+  end
+  it 'can log out as a registered user' do
+    user = User.create(username: "Chros", password: "4p9ybaos")
+    visit '/'
+    click_on "I already have an account"
+
+    expect(current_path).to eq(login_path)
+    fill_in "username", with: user.username
+    fill_in "password", with: user.password
+
+    click_on "Log In"
+
+    expect(current_path).to eq(user_path(user))
+
+    click_on "Log Out"
+
+    expect(current_path).to eq(root_path)
   end
 end
