3b678745067b39011b79fbd2fd2d927613fbbfe3
Chris Powell <chris.ian.powell@gmail.com>
2018-07-17 19:21:02 -0600

    adds favorites relationships to an artwork and user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
  class User < ApplicationRecord
   validates :username, uniqueness: true, presence: true
   validates_presence_of :password, require: true
+  
+  has_many :favorites
+  has_many :artworks, through: :favorites
 
   enum role: %w(default admin)
 
